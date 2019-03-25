6b2ab3df4432887a160dbd9839c715a7646b294d
duongbaongoc <ngoc.duong@wsu.edu>
2019-03-25 15:22:29 -0700

    get pip

---
+++ Do this after you download the program - One  time set up/Step2 - double click on this to instlal paramiko and pscp - Copy.py
@@ -0,0 +1,31 @@
+import os
+from tkinter import *
+from tkinter import messagebox
+
+
+root = Tk()
+root.withdraw()
+
+
+#pip
+failure = os.system("python get-pip.py")
+if (failure):
+	messagebox.showerror("Error", "Failed to install pip")
+	exit()
+
+
+#paramiko
+failure = os.system("pip install paramiko")
+if (failure):
+	messagebox.showerror("Error", "Failed to install paramiko")
+	exit()
+
+
+#pscp
+failure = os.system("python -m pip install pscp")
+if (failure):
+	messagebox.showerror("Error", "Failed to install pscp")
+	exit()
+	
+	
+messagebox.showinfo("Congrats!", "Successfully installed all dependencies (Python, paramiko, pscp)")
